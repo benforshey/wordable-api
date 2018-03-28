b647810d831cdbf8977dad31f7edc7938815749c
Ben Forshey <hello@integrisweb.com>
2018-03-28 09:50:32 -0400

    correct comments; adjust cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const wordableAPI = {
   shouldBlock: false,
   callsLeft: 0,
 
-  async call(url = '/words/?random=true') {
+  async call({ url = '/words/?random=true', buffer = 50 } = {}) {
     const response = await fetch(`https://wordsapiv1.p.mashape.com${url}`, {
       method: 'GET',
       headers: {
@@ -19,18 +19,19 @@ const wordableAPI = {
     const { headers, status } = await response;
     const json = await response.json();
 
-    this.updateLimits({ response });
+    this.updateLimits({ buffer, response });
 
     return { headers, status, json };
   },
 
   async updateLimits({ buffer = 50, response } = {}) {
-    console.log('update limits called');
     try {
-      const { headers } = response || await this.call();
+      const { headers } = response || await this.call({ buffer });
       const remaining = headers.get('X-Ratelimit-Requests-Remaining');
       const callsLeft = Number.parseInt(remaining, 10) - Number.parseInt(buffer, 10);
       const shouldBlock = callsLeft <= buffer;
+
+      console.log(callsLeft);
 
       this.shouldBlock = shouldBlock;
       this.callsLeft = callsLeft;
@@ -42,14 +43,14 @@ const wordableAPI = {
 
 // NOTE: This is an async call, but its ok for a request to come before
 // this updateLimits call, because the .call() method also updates limits.
-// Set a timeout to check the limit again in 30 minutes (48 wasted calls per day).
+// Set an interval to check the limit again in 30 minutes (48 wasted calls per day).
 // Still better than tying a setTimeout to the request, which effectively
 // enables a built-in denial of service just by calling a maxed out API.
 setInterval(() => wordableAPI.updateLimits({ buffer: 250 }), 1000 * 60 * 30);
 
 const micro = async (req, res) => {
   // TODO: set to my wordable URL (once I figure out where to put this thing).
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   try {
     if (wordableAPI.shouldBlock) {
       res.setHeader('Retry-After', '120');
@@ -57,7 +58,7 @@ const micro = async (req, res) => {
       return send(res, 429, 'API Limit Exceeded');
     }
 
-    const { json, status } = await wordableAPI.call(req.url);
+    const { json, status } = await wordableAPI.call({ url: req.url, buffer: 250 });
 
     return send(res, status, json);
   } catch (err) {
